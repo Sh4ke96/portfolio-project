678caa637c0375a075b2904f556846a22e4309db
sh4ke96 <sh4ke96@gmail.com>
2022-12-17 21:23:15 +0100

    change section experience and section projects RWD

--- components/Sections/SectionExperience.tsx
+++ components/Sections/SectionExperience.tsx
@@ -8,9 +8,9 @@ const SectionExperience: FC = () => {
       className="container mx-auto flex flex-col items-center justify-center px-5 py-16"
     >
       <Heading title="experience" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-7 mb-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 items-start gap-7 mb-4">
         <div className="col-span-1 flex flex-col justify-center items-center">
-          <div className="w-1/3 border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
+          <div className="whitespace-nowrap border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
             09.2022 - Present
           </div>
           <h5 className="text-green text-xl font-bold">Frontend Developer</h5>
@@ -22,7 +22,7 @@ const SectionExperience: FC = () => {
           </p>
         </div>
         <div className="col-span-1 flex flex-col justify-center items-center">
-          <div className="w-1/3 border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
+          <div className="whitespace-nowrap border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
             02.2022 - 06.2022
           </div>
           <h5 className="text-green text-xl font-bold">Frontend Developer</h5>
@@ -33,7 +33,7 @@ const SectionExperience: FC = () => {
           </p>
         </div>
         <div className="col-span-1 flex flex-col justify-center items-center">
-          <div className="w-1/3 border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
+          <div className="whitespace-nowrap border border-1 border-gray border-opacity-10 text-center rounded-lg p-2 md:p-3 bg-light_blue mb-4">
             10.2021 - 12.2021
           </div>
           <h5 className="text-green text-xl font-bold">Support</h5>
--- components/Sections/SectionProjects.tsx
+++ components/Sections/SectionProjects.tsx
@@ -10,8 +10,8 @@ const SectionProjects: FC = () => {
       className="container mx-auto flex flex-col items-center justify-center px-5 py-16"
     >
       <Heading title="projects" />
-      <div className="grid grid-cols-6 gap-7 mb-8">
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+      <div className="grid grid-cols-4 gap-7 mb-8">
+        <div className="col-span-4 lg:col-span-2">
           <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
             <Image
               width={450}
@@ -36,32 +36,7 @@ const SectionProjects: FC = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-          <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
-            <Image
-              width={450}
-              height={315}
-              className="mb-2"
-              src="/images/portfolio-image-1.jpg"
-              alt="coconut oil"
-            />
-            <div className="flex items-center justify-between pt-4">
-              <div>
-                <h5 className="font-bold text-white text-xl">Lorem Ipsum</h5>
-                <p className="text-gray text-lg">Lorem ipsum</p>
-              </div>
-              <div className="flex">
-                <a href="#">
-                  <RiShareBoxFill className="text-6xl p-2 fill-gray hover:fill-green delay-100" />
-                </a>
-                <a href="#">
-                  <RiGithubFill className="text-6xl p-2 fill-gray hover:fill-green delay-100" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+        <div className="col-span-4 lg:col-span-2">
           <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
             <Image
               width={450}
@@ -87,8 +62,8 @@ const SectionProjects: FC = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-6 gap-7">
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+      <div className="grid grid-cols-4 gap-7">
+        <div className="col-span-4 lg:col-span-2">
           <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
             <Image
               width={450}
@@ -113,32 +88,7 @@ const SectionProjects: FC = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-          <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
-            <Image
-              width={450}
-              height={315}
-              className="mb-2"
-              src="/images/portfolio-image-1.jpg"
-              alt="coconut oil"
-            />
-            <div className="flex items-center justify-between pt-4">
-              <div>
-                <h5 className="font-bold text-white text-xl">Lorem Ipsum</h5>
-                <p className="text-gray text-lg">Lorem ipsum</p>
-              </div>
-              <div className="flex">
-                <a href="#">
-                  <RiShareBoxFill className="text-6xl p-2 fill-gray hover:fill-green delay-100" />
-                </a>
-                <a href="#">
-                  <RiGithubFill className="text-6xl p-2 fill-gray hover:fill-green delay-100" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+        <div className="col-span-4 lg:col-span-2">
           <div className="border border-1 border-gray border-opacity-10 rounded-lg p-4 md:p-5 bg-light_blue">
             <Image
               width={450}
