964d36d1be85a07af62a5bc0c268998273827b6d
sh4ke96 <sh4ke96@gmail.com>
2022-12-18 22:34:08 +0100

    change favicon, add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Artur Michałek - Frontend Developer</title>
         <meta name="description" content="Frontend Developer" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
