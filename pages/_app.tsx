03d2c54ff70b0839b647fdc47d785157dd185fdd
sh4ke96 <sh4ke96@gmail.com>
2023-09-11 19:09:27 +0200

    upgrade next and other packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const poppins = Poppins({
+  weight: ["100", "200", "400", "700", "900"],
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div id="root" className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-export default MyApp
